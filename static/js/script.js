3e472f81e2c5fe28feb8422b344da5f2c5a01bf6
NullMC <franciscopperfield8@gmail.com>
2024-09-29 22:32:51 +0200

    added "or" in the whiles and added string interpolation

--- static/js/script.js
+++ static/js/script.js
@@ -1,70 +1,51 @@
-let RAcounter = 0 //contantori
-let WAcounter = 0
+let RAcounter = 0; //contatori
+let WAcounter = 0;
 
-let var1V = "francesco"
-let var2V = "si"
-let var3V = "bari"
+let var1V = "francesco";
+let var2V = "si";
+let var3V = "bari";
 
-let var1 = prompt("Come si chiama il creatore di questo sito? -> ")
-if(var1 === null) {
-    // codice da eseguire se la condizione è vera
-    while(var1 === null)
-    {
-        var1 = prompt("completa il campo richiesto: ")
-    }
+let var1 = prompt("Come si chiama il creatore di questo sito? -> ");
+while (var1 === null || var1 === "") {
+    var1 = prompt("Completare il campo richiesto: ");
 }
-var1 = var1.toLocaleLowerCase()
-var1 = var1.trim()
+var1 = var1.toLocaleLowerCase();
+var1 = var1.trim();
 
-if(var1 === var1V){
-    RAcounter++
-    alert("risposta esatta")
-}
-else{
-    WAcounter++
-    alert("risposta sbagliata, risposta corretta: ", + var1V)
+if (var1 === var1V) {
+    RAcounter++;
+    alert("Risposta esatta");
+} else {
+    WAcounter++;
+    alert(`Risposta sbagliata, risposta corretta: ${var1V}`);
 }
 
-
-
-let var2 = prompt("Prof. Mastrandrea è il migliore? (si/no) -> ")
-if(var2 === null) {
-    // codice da eseguire se la condizione è vera
-    while(var2 === null)
-    {
-        var2 = prompt("completa il campo richiesto: ")
-    }
+let var2 = prompt("Prof. Mastrandrea è il migliore? (si/no) -> ");
+while (var2 === null || var2 === "") {
+    var2 = prompt("Completare il campo richiesto: ");
 }
-var2 = var2.toLocaleLowerCase()
-var2 = var2.trim()
+var2 = var2.toLocaleLowerCase();
+var2 = var2.trim();
 
-if(var2 === var2V){
-    RAcounter++
-    alert("risposta esatta")
-}
-else{
-    WAcounter++
-    alert("risposta sbagliata, risposta corretta: ", + var2V)
+if (var2 === var2V) {
+    RAcounter++;
+    alert("Risposta esatta");
+} else {
+    WAcounter++;
+    alert(`Risposta sbagliata, risposta corretta: ${var2V}`);
 }
 
-
-
-let var3 = prompt("In che provincia si trova Bitritto? -> ")
-if(var3 === null) {
-    // codice da eseguire se la condizione è vera
-    while(var3 === null)
-    {
-        var3 = prompt("completa il campo richiesto: ")
-    }
+let var3 = prompt("In che provincia si trova Bitritto? -> ");
+while (var3 === null || var3 === "") {
+    var3 = prompt("Completare il campo richiesto: ");
 }
-var3 = var3.toLocaleLowerCase()
-var3 = var3.trim()
+var3 = var3.toLocaleLowerCase();
+var3 = var3.trim();
 
-if(var3 === var3V){
-    RAcounter++
-    alert("risposta esatta")
-}
-else{
-    WAcounter++
-    alert("risposta sbagliata, risposta corretta: ", + var3V)
+if (var3 === var3V) {
+    RAcounter++;
+    alert("Risposta esatta");
+} else {
+    WAcounter++;
+    alert(`Risposta sbagliata, risposta corretta: ${var3V}`);
 }
