78f13d187324a48a65d691e7691cf2cb957a824f
NullMC <franciscopperfield8@gmail.com>
2024-09-29 23:09:20 +0200

    form completed

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,12 @@ function startQuiz(){
     let var1V = "francesco";
     let var2V = "si";
     let var3V = "bari";
+    let var4V = "russia";
+    let var5V = "tokyo";
+    let var6V = "mattarella";
+    let var7V = "australia";
+
+
 
     let var1 = prompt("Come si chiama il creatore di questo sito? -> ");
     while (var1 === null || var1 === "") {
@@ -21,6 +27,8 @@ function startQuiz(){
         alert(`Risposta sbagliata, risposta corretta: ${var1V}`);
     }
 
+
+
     let var2 = prompt("Prof. Mastrandrea è il migliore? (si/no) -> ");
     while (var2 === null || var2 === "") {
         var2 = prompt("Completare il campo richiesto: ");
@@ -36,6 +44,8 @@ function startQuiz(){
         alert(`Risposta sbagliata, risposta corretta: ${var2V}`);
     }
 
+
+
     let var3 = prompt("In che provincia si trova Bitritto? -> ");
     while (var3 === null || var3 === "") {
         var3 = prompt("Completare il campo richiesto: ");
@@ -50,4 +60,79 @@ function startQuiz(){
         WAcounter++;
         alert(`Risposta sbagliata, risposta corretta: ${var3V}`);
     }
+
+
+
+    let var4 = prompt("Qual é la nazione piú grande al mondo? -> ");
+    while (var4 === null || var4 === "") {
+        var4 = prompt("Completare il campo richiesto: ");
+    }
+    var4 = var4.toLocaleLowerCase();
+    var4 = var4.trim();
+
+    if (var4 === var4V) {
+        RAcounter++;
+        alert("Risposta esatta");
+    } else {
+        WAcounter++;
+        alert(`Risposta sbagliata, risposta corretta: ${var4V}`);
+    }
+
+
+
+    let var5 = prompt("Qual é la cittá piú tecnologica del mondo? -> ");
+    while (var5 === null || var5 === "") {
+        var5 = prompt("Completare il campo richiesto: ");
+    }
+    var5 = var5.toLocaleLowerCase();
+    var5 = var5.trim();
+
+    if (var5 === var5V) {
+        RAcounter++;
+        alert("Risposta esatta");
+    } else {
+        WAcounter++;
+        alert(`Risposta sbagliata, risposta corretta: ${var5V}`);
+    }
+
+
+
+    let var6 = prompt("Come si chiama il presidente della repubblica italiana? -> ");
+    while (var6 === null || var6 === "") {
+        var1 = prompt("Completare il campo richiesto: ");
+    }
+    var6 = var6.toLocaleLowerCase();
+    var6 = var6.trim();
+
+    if (var6 === var6V) {
+        RAcounter++;
+        alert("Risposta esatta");
+    } else {
+        WAcounter++;
+        alert(`Risposta sbagliata, risposta corretta: ${var6V}`);
+    }
+
+
+
+    let var7 = prompt("Come si chiama il creatore di questo sito? -> ");
+    while (var7 === null || var7 === "") {
+        var7 = prompt("Completare il campo richiesto: ");
+    }
+    var7 = var7.toLocaleLowerCase();
+    var7 = var7.trim();
+
+    if (var7 === var7) {
+        RAcounter++;
+        alert("Risposta esatta");
+    } else {
+        WAcounter++;
+        alert(`Risposta sbagliata, risposta corretta: ${var7}`);
+    }
+
+
+    let totaleDomande = 7;
+    let percentualeCorrette = (RAcounter / totaleDomande) * 100;
+    let percentualeSbagliate = (WAcounter / totaleDomande) * 100;
+
+    alert(`Risultato finale: ${RAcounter} risposte esatte (${percentualeCorrette.toFixed(2)}%) e ${WAcounter} risposte sbagliate (${percentualeSbagliate.toFixed(2)}%)`);
 }
